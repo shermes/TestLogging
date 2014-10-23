d7a7645a5d8f314c2a32ae659fad1b929a78a17c
Scott Hermes <shermes@solstice-consulting.com>
2014-10-23 15:23:04 -0500

    Completed POC

--- TestLogger/ViewController.m
+++ TestLogger/ViewController.m
@@ -8,6 +8,7 @@
 
 #import "ViewController.h"
 #import <CocoaLumberjack.h>
+#import <Crashlytics/Crashlytics.h>
 @interface ViewController ()
 
 @end
@@ -16,11 +17,21 @@
 - (IBAction)buttonClick:(id)sender {
     // Call once
     [CoreLogger NTLogError:@"This is an error"];
+    CLSLog(@"This is a message from CLSLog");
+    CLSNSLog(@"This is a message from CLS NS Log");
+    [[Crashlytics sharedInstance] crash];
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [Crashlytics startWithAPIKey:@"a12568c0b92f1f19a8e79e4187c23ca28bacfec3"];
     // Do any additional setup after loading the view, typically from a nib.
+    [CoreLogger NTLogVerbose:@"This is JUST A MESSAGE"];
+    // NSError* error;
+    NSString *employeeJSONPath = [[NSBundle mainBundle] pathForResource:@"offlineUserLayouts" ofType:@"json"];
+    NSString *employeeJSON = [NSString stringWithContentsOfFile:employeeJSONPath encoding:NSUTF8StringEncoding error:NULL];
+//    NSData *data = [employeeJSON dataUsingEncoding:NSUTF8StringEncoding];
+    [CoreLogger NTLogVerbose:employeeJSON];
 }
 
 - (void)didReceiveMemoryWarning {
